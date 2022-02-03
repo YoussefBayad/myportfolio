c88144b46dfaf9a28e08b3d0dc83e410298e9394
Joseph bayad <jobayad66@gmail.com>
2022-02-03 14:44:55 +0100

    adding three of my projects to project section

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -20,24 +20,34 @@ export const projects = [
     id: 1,
   },
   {
-    title: 'WebRTC App',
+    title: 'Homie app',
     description:
-      "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+      'Full-stack, Real time, fully-featured , Responsive social media application using React, Firebase, Redux Toolkit,Sass, Formik, Framer Motion. when you add or like or use any of the functionality every user receive it in real time To log in: make any email you want and password longer than 6 characters',
     image: '/images/3.jpg',
-    tags: ['React', 'WebRTC'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    tags: ['React', 'Chat', 'CRUD', 'Real time'],
+    source: 'https://github.com/YoussefBayad/Homie-firebase',
+    visit: 'http://homiees.web.app/',
     id: 2,
   },
   {
-    title: 'Unichat',
+    title: 'Arabic Universe',
     description:
-      'This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application',
+      'This is a modern design website for an agency that teach arabic , iT a fast optimized "SEO" website build with the latest technologies Nextjs React Framer motion and theme UI',
     image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    tags: ['Next js', 'React', 'UI/UX'],
+    source: 'https://github.com/YoussefBayad/fouzia-arabicuniverse',
+    visit: 'https://arabicuniverse.vercel.app/',
     id: 3,
+  },
+  {
+    title: 'E-Commerce',
+    description:
+      'Full-stack,Real time MERN e-commerce web app using React, Nodejs, Express, Mongodb Redux Toolkit,Sass, Formik, Framer Motion.To signup in: make any email you want and password longer than 6 characters',
+    image: '/images/5.png',
+    tags: ['MERN', 'React', 'Nodejs', 'mongodb'],
+    source: 'https://github.com/YoussefBayad/eb-firebase',
+    visit: 'https://eb-ecommerce.web.app/shop',
+    id: 4,
   },
 ];
 
