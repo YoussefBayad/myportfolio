c7372da5adf02841f46af3bd048b278f1542efac
Joseph bayad <jobayad66@gmail.com>
2022-02-03 15:02:42 +0100

    add my fifth project to constent

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -52,11 +52,12 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey' },
-  { year: 2018, text: 'Worked as a freelance developer' },
-  { year: 2019, text: 'Founded JavaScript Mastery' },
+  { year: 2019, text: 'Started my journey' },
   { year: 2020, text: 'Shared my projects with the world' },
-  { year: 2021, text: 'Started my own platform' },
+  {
+    year: 2021,
+    text: 'I enrolled in a lot of internships and build some amazing websites ',
+  },
 ];
 
 export const SocialLinks = {
