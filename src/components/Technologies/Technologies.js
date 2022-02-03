5af3def4618a4b5fa2a572e10ffc7d2787494208
Joseph bayad <jobayad66@gmail.com>
2022-02-03 15:01:52 +0100

    edit tech section content

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,32 +1,44 @@
 import React from 'react';
 import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <Section id="tech">
+const Technologies = () => (
+  <Section id='tech'>
     <SectionDivider divider />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
-      I've worked with a range a technologies in the web development world.
-      From Back-end To Design
+      I've worked with a range a technologies in the web development world. From
+      Back-end To Design
     </SectionText>
     <List>
       <ListItem>
         <picture>
-          <DiReact size="3rem" />
+          <DiReact size='3rem' />
         </picture>
         <ListContainer>
           <ListTitle>Front-End</ListTitle>
           <ListParagraph>
-            Experiece with <br />
-            React.js
+            Experience with <br />
+            React.js, Next.js, ThemeUI, Bootstrap, Redux, web animation and many
+            more...
           </ListParagraph>
         </ListContainer>
       </ListItem>
       <ListItem>
         <picture>
-          <DiFirebase size="3rem" />
+          <DiFirebase size='3rem' />
         </picture>
         <ListContainer>
           <ListTitle>Back-End</ListTitle>
@@ -38,7 +50,7 @@ const Technologies = () =>  (
       </ListItem>
       <ListItem>
         <picture>
-          <DiZend size="3rem" />
+          <DiZend size='3rem' />
         </picture>
         <ListContainer>
           <ListTitle>UI/UX</ListTitle>
