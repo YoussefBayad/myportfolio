a79211d19488900aae5d7c6c7008d090a55fa504
Joseph bayad <jobayad66@gmail.com>
2022-02-03 15:05:21 +0100

    edit tech acomplishment  content

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  { number: 10, text: 'plus Open Source Projects' },
+  { number: 20, text: 'plus Students' },
+  { number: 50, text: 'plus website' },
+  // { number: 5000, text: 'Github Stars', }
 ];
 
 const Acomplishments = () => (
@@ -21,7 +25,7 @@ const Acomplishments = () => (
         </Box>
       ))}
     </Boxes>
-    <SectionDivider/>
+    <SectionDivider />
   </Section>
 );
 
