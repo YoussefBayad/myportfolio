5c4b766062ab3f723f8e9e8a1e9f854befaa74bf
Joseph bayad <jobayad66@gmail.com>
2022-02-05 18:40:49 +0100

    add sticky to layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Sticky from 'react-stickynode';
 
-import Footer from '../components/Footer/Footer'
-import Header from '../components/Header/Header'
-import { Container } from './LayoutStyles'
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
+import { Container } from './LayoutStyles';
 
-export const Layout = ({children}) => {
+export const Layout = ({ children }) => {
+  const [isSticky, setIsSticky] = useState(false);
+
+  const handleStateChange = (status) => {
+    if (status.status === Sticky.STATUS_FIXED) {
+      setIsSticky(true);
+    } else if (status.status === Sticky.STATUS_ORIGINAL) {
+      setIsSticky(false);
+    }
+  };
+
   return (
     <Container>
-     <Header/>
-     <main>{children}</main> 
-     <Footer/>
+      <Sticky innerZ={1001} top={0} onStateChange={handleStateChange}>
+        <Header className={`${isSticky ? 'sticky' : 'unSticky'}`} />
+      </Sticky>
+      <main>{children}</main>
+      <Footer />
     </Container>
-  )
-}
+  );
+};
