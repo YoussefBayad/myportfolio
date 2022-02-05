52048fd142ccd82cd7e6b237e51a5dd19a5b2ef5
Joseph bayad <jobayad66@gmail.com>
2022-02-05 18:44:13 +0100

    drawer component

---
+++ src/components/mobileDrawer/drawer.js
@@ -0,0 +1,46 @@
+import React, { Fragment } from 'react';
+import RcDrawer from 'rc-drawer';
+
+export default function Drawer({
+  className,
+  children,
+  closeButton,
+  closeButtonStyle,
+  drawerHandler,
+  toggleHandler,
+  open,
+  width,
+  placement,
+  DrawerBox,
+  Close,
+  ...props
+}) {
+  return (
+    <>
+      <RcDrawer
+        open={open}
+        onClose={toggleHandler}
+        className={`drawer ${className || ''}`.trim()}
+        width={width}
+        placement={placement}
+        handler={false}
+        duration={'0.4s'}
+        {...props}
+        style={{ backgroundColor: 'white' }}>
+        {Close && <Close onClick={toggleHandler}>{closeButton}</Close>}
+        <DrawerBox>{children} </DrawerBox>
+      </RcDrawer>
+      <div
+        className='drawer__handler'
+        style={{ display: 'inline-block' }}
+        onClick={toggleHandler}>
+        {drawerHandler}
+      </div>
+    </>
+  );
+}
+
+Drawer.defaultProps = {
+  width: '320px',
+  placement: 'left',
+};
