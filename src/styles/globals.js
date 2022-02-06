fd449378cf57e7378e12a3eba4993283278b21fc
Joseph bayad <jobayad66@gmail.com>
2022-02-06 15:13:41 +0100

    change header colors when its sticky

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -15,15 +15,27 @@ const GlobalStyles = createGlobalStyle`
 
   }
   body {
-    font-family: ${props => props.theme.fonts.main};
+    font-family: ${(props) => props.theme.fonts.main};
     font-size: 1.6rem;
-    background: ${props => props.theme.colors.background1};
-    color: ${props => props.theme.colors.primary1};
+    background: ${(props) => props.theme.colors.background1};
+    color: ${(props) => props.theme.colors.primary1};
     cursor: default;
 
   }
+ 
+  .sticky {
+    background-color:hsl(232.7,27.3%,33.7%);
+/* 
+    primary1: "hsl(204,23.8%,95.9%)",
+    background1: "#0F1624",
+    accent1: "hsl(34.9,98.6%,72.9%)",
+    button: "hsl(205.1,100%,36.1%)",
+    background2: "hsl(232.7,27.3%,23.7%)", */
+    box-shadow: 0 1px 15px hsl(205.1,100%,36.1%);
+    
+  }
   h1,h2,h3,h4,h5,h6,button {
-    font-family: ${props => props.theme.fonts.title};
+    font-family: ${(props) => props.theme.fonts.title};
   }
   a {
     text-decoration: none;
