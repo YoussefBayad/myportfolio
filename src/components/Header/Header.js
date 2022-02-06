969c67913a80c676a0a247177a2c0316b7e08005
Joseph bayad <jobayad66@gmail.com>
2022-02-06 15:13:16 +0100

    adjust header style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,7 @@ import { SocialLinks } from '../../constants/constants';
 import NavDropDown from '../NavDropDown';
 import MobileDrawer from '../mobileDrawer';
 import {
+  Header,
   Container,
   Div1,
   Div2,
@@ -14,50 +15,50 @@ import {
   SocialIcons,
 } from './HeaderStyles';
 
-const Header = ({ className }) => (
-  <Container className={className} id='headers'>
-    <Div1>
-      <Link href='/'>
-        <a style={{ display: 'flex', alignItems: 'center', color: 'white' }}>
-          <DiCssdeck size='3rem' /> <span>Portfolio</span>
-        </a>
-      </Link>
-    </Div1>
-    <Div2>
-      <li>
-        <Link href='#projects'>
-          <NavLink>Projects</NavLink>
+export default ({ className }) => (
+  <Header className={className}>
+    <Container>
+      <Div1>
+        <Link href='/'>
+          <a style={{ display: 'flex', alignItems: 'center', color: 'white' }}>
+            <DiCssdeck size='3rem' /> <span>Portfolio</span>
+          </a>
         </Link>
-      </li>
-      <li>
-        <Link href='#contact'>
-          <NavLink>Contact</NavLink>
-        </Link>
-      </li>
-      <li>
-        <Link href='#tech'>
-          <NavLink>Technologies</NavLink>
-        </Link>
-      </li>
-      <li>
-        <Link href='#about'>
-          <NavLink>About</NavLink>
-        </Link>
-      </li>
-    </Div2>
-    <Div3>
-      <SocialIcons href={SocialLinks.github}>
-        <AiFillGithub size='3rem' />
-      </SocialIcons>
-      <SocialIcons href={SocialLinks.linkedin}>
-        <AiFillLinkedin size='3rem' />
-      </SocialIcons>
-      <SocialIcons href={SocialLinks.instagram}>
-        <AiFillInstagram size='3rem' />
-      </SocialIcons>
-    </Div3>
-    <MobileDrawer />
-  </Container>
+      </Div1>
+      <Div2>
+        <li>
+          <Link href='#projects'>
+            <NavLink>Projects</NavLink>
+          </Link>
+        </li>
+        <li>
+          <Link href='#contact'>
+            <NavLink>Contact</NavLink>
+          </Link>
+        </li>
+        <li>
+          <Link href='#tech'>
+            <NavLink>Technologies</NavLink>
+          </Link>
+        </li>
+        <li>
+          <Link href='#about'>
+            <NavLink>About</NavLink>
+          </Link>
+        </li>
+      </Div2>
+      <Div3>
+        <SocialIcons href={SocialLinks.github}>
+          <AiFillGithub size='3rem' />
+        </SocialIcons>
+        <SocialIcons href={SocialLinks.linkedin}>
+          <AiFillLinkedin size='3rem' />
+        </SocialIcons>
+        <SocialIcons href={SocialLinks.instagram}>
+          <AiFillInstagram size='3rem' />
+        </SocialIcons>
+      </Div3>
+      {/* <MobileDrawer /> */}
+    </Container>
+  </Header>
 );
-
-export default Header;
