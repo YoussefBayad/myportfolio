41a8b404d5c47acf9851f7c4ffe3ed7acf32e227
Joseph bayad <jobayad66@gmail.com>
2022-02-07 03:03:12 +0100

    contact section layout

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from './styles';
+
+const Contact = () => (
+  <Section id='contact'>
+    <SectionTitle>Contact</SectionTitle>
+    <SectionText>
+      I've worked with a range a Contact in the web development world. From
+      Back-end To Design
+    </SectionText>
+    <List>
+      <ListItem>
+        <picture>
+          <DiReact size='3rem' />
+        </picture>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js, Next.js, ThemeUI, Bootstrap, Redux, web animation and many
+            more...
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <DiFirebase size='3rem' />
+        </picture>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node and Databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <DiZend size='3rem' />
+        </picture>
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            tools like Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+    <SectionDivider colorAlt />
+  </Section>
+);
+
+export default Contact;
--- src/components/mobileDrawer/drawer.js
+++ src/components/mobileDrawer/drawer.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import RcDrawer from 'rc-drawer';
 
 export default function Drawer({
