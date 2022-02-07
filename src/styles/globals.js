66bf07589ed894e810a5d266671c8303e9e899c9
Joseph bayad <jobayad66@gmail.com>
2022-02-07 03:02:13 +0100

    header background color

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -24,14 +24,8 @@ const GlobalStyles = createGlobalStyle`
   }
  
   .sticky {
-    background-color:hsl(232.7,27.3%,33.7%);
-/* 
-    primary1: "hsl(204,23.8%,95.9%)",
-    background1: "#0F1624",
-    accent1: "hsl(34.9,98.6%,72.9%)",
-    button: "hsl(205.1,100%,36.1%)",
-    background2: "hsl(232.7,27.3%,23.7%)", */
-    box-shadow: 0 1px 15px hsl(205.1,100%,36.1%);
+    background-color:hsl(232.7,27.3%,23.7%);
+    box-shadow: 0 1px 15px hsl(205.1,100%,20.1%);
     
   }
   h1,h2,h3,h4,h5,h6,button {
