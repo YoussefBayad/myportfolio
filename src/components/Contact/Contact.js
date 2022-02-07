8c9d861f03c7d8ad3ef0f8862a444cb52d0a1396
Joseph bayad <jobayad66@gmail.com>
2022-02-07 03:16:02 +0100

    contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,6 +6,7 @@ import {
   SectionText,
   SectionTitle,
 } from '../../styles/GlobalComponents';
+import NavDropDown from '../NavDropDown';
 import {
   List,
   ListContainer,
@@ -17,49 +18,7 @@ import {
 const Contact = () => (
   <Section id='contact'>
     <SectionTitle>Contact</SectionTitle>
-    <SectionText>
-      I've worked with a range a Contact in the web development world. From
-      Back-end To Design
-    </SectionText>
-    <List>
-      <ListItem>
-        <picture>
-          <DiReact size='3rem' />
-        </picture>
-        <ListContainer>
-          <ListTitle>Front-End</ListTitle>
-          <ListParagraph>
-            Experience with <br />
-            React.js, Next.js, ThemeUI, Bootstrap, Redux, web animation and many
-            more...
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-      <ListItem>
-        <picture>
-          <DiFirebase size='3rem' />
-        </picture>
-        <ListContainer>
-          <ListTitle>Back-End</ListTitle>
-          <ListParagraph>
-            Experience with <br />
-            Node and Databases
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-      <ListItem>
-        <picture>
-          <DiZend size='3rem' />
-        </picture>
-        <ListContainer>
-          <ListTitle>UI/UX</ListTitle>
-          <ListParagraph>
-            Experience with <br />
-            tools like Figma
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-    </List>
+    <NavDropDown isOpen={true} />
     <SectionDivider colorAlt />
   </Section>
 );
