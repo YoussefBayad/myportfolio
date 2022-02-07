fcd0e1308157a407a66089f8422459617aa5cfd4
Joseph bayad <jobayad66@gmail.com>
2022-02-07 03:01:59 +0100

    header data as constant

---
+++ src/constants/header.data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: 'projects',
+    label: 'Projects',
+  },
+  {
+    path: 'tech',
+    label: 'Tech',
+  },
+  {
+    path: 'about',
+    label: 'About',
+  },
+  {
+    path: 'contact',
+    label: 'Contact',
+  },
+];
