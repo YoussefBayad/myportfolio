02af5904fb65dcefbe7a2da124e2dda07bc9c3fa
YoussefBayad <jobayad66@gmail.com>
2022-02-18 15:31:23 +0100

    improve seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { StickyProvider } from '../contexts/app/app.provider';
-import Acomplishments from '../components/Acomplishments/Acomplishments';
-import Contact from '../components/Contact/Contact';
-import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
-import Hero from '../components/Hero/Hero';
-import Projects from '../components/Projects/Projects';
-import Technologies from '../components/Technologies/Technologies';
-import Timeline from '../components/TimeLine/TimeLine';
-import { Layout } from '../layout/Layout';
-import { Section } from '../styles/GlobalComponents';
+import React from "react";
+import { StickyProvider } from "../contexts/app/app.provider";
+import Acomplishments from "../components/Acomplishments/Acomplishments";
+import Contact from "../components/Contact/Contact";
+import BgAnimation from "../components/BackgrooundAnimation/BackgroundAnimation";
+import Hero from "../components/Hero/Hero";
+import SEO from "../components/SEO";
+import Projects from "../components/Projects/Projects";
+import Technologies from "../components/Technologies/Technologies";
+import Timeline from "../components/TimeLine/TimeLine";
+import { Layout } from "../layout/Layout";
+import { Section } from "../styles/GlobalComponents";
 
 const Home = () => {
   return (
     <StickyProvider>
       <Layout>
+        <SEO />
         <Section grid>
           <Hero />
           <BgAnimation />
